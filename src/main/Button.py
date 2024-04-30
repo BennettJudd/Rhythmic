46608a59fa5001ee5c9c4dd2d67fc25305dea24f
BennettJudd <142922052+BennettJudd@users.noreply.github.com>
2024-04-30 09:15:42 -0600

    Update Button.py

--- src/main/Button.py
+++ src/main/Button.py
@@ -1,7 +1,8 @@
+
 import pygame
 
 class Button:
-    def __init__(self,x,y,w,h,r,b,g):
+    def __init__(self,x,y,w,h,r,b,g,over):
         self.x = x
         self.y = y
         self.w = w
@@ -9,9 +10,21 @@ class Button:
         self.r = r
         self.b = b
         self.g = g
-    
+        self.over = False
 
     def create_rect(self, surface):
         return pygame.draw.rect(surface, (self.r,self.b,self.g), [self.x, self.y, self.w, self.h])
+
+
+    def is_over(self,x,y):
+        if x > self.x and y > self.y and x < (self.x + self.w) and y < (self.y + self.h):
+            print("yes")  
+            self.over = True
+           
+            
+        else:
+            print(self.x)
+            self.over = False
+
 
 
