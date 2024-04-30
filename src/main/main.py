922b7d99913a9b03387c2de1bec83058059ea7e6
BennettJudd <142922052+BennettJudd@users.noreply.github.com>
2024-04-30 09:15:06 -0600

    Update main.py

--- src/main/main.py
+++ src/main/main.py
@@ -12,44 +12,81 @@ font = pygame.font.Font(None, 36)
 
 rbg = 1
 rbgmult = 1
+rbgt = 1
+rbgtmult = 1
+b = 210
+g = 210
+bu = 255
+gu = 255
 SS = True
 NP = True
+Menu = False
 pygame.display.set_caption('~-Rhythmic-~')
 while True:
+    pygame.display.update()
     for event in pygame.event.get():
-        color = (0,0,0)
+
+        color = (20,20,20)
         surface.fill(color)
-        
-    if NP == True:     
-       
+        if event.type == pygame.MOUSEMOTION:
+            mousex, mousey = event.pos
+            
+    if NP == True:
+
+
+        rbg += .3 * rbgmult
+
+        if rbg >= 255 or rbg <= 1:
+            rbgmult *= -1
+        rbgt += .3 * rbgtmult
+
+        if rbgt >= 255 or rbgt <= 1:
+            rbgtmult *= -1   
+
         if SS == True:
 
-        
+            
+            
           
-            rbg += .3 * rbgmult
+            
+           
+            
 
-            if rbg >= 255 or rbg <= 1:
-                rbgmult *= -1           
-            pygame.draw.rect(surface,(rbg,200,200),[10,10,580,580],10)
-        
-
-            b1 = Button(250,300,100,20,rbg,210,210)
+            b1 = Button(250,300,100,20,rbgt,b,g,False)
             b1.create_rect(surface)
-     
-
-            text = font.render('Press any key to start the game', True, (255, 255, 255))
-            text_rect = text.get_rect(center=(1920//2, 1080//2 + 100))
-            surface.blit(text, text_rect)  
-      
-        
-        
-            if event.type == pygame.MOUSEBUTTONUP:
-                SS = False
-
-
-    pygame.display.update()
+            b1.is_over(mousex, mousey)
+            if b1.over == True:
+                print("hi")
+                b = bu
+                g = gu
+                rbgt = 210
+               
+                if event.type == pygame.MOUSEBUTTONUP:
+                    SS = False
+                    Menu = True
+                else:
+                    b = 210
+                    g = 210
+            
+            text = font.render('Start', True, (0, 0, 0))
+            surface.blit(text, (272, 298))
+        pygame.draw.rect(surface,(rbg,200,200),[10,10,580,580],10)
+ 
+       
+        if Menu == True:
+            b2 = Button(100,175,75,75,rbgt,b,g,False)
+            b2.create_rect(surface)
+            b2.is_over(mousex, mousey)
+            if b2.over == True:
+                print("hi")
+                b = bu
+                g = gu
+                rbgt = 210
+    
+    
     
     if event.type == QUIT:
-        pygame.quit()
-        sys.exit()
-pygame.display.update()
+         pygame.quit()
+         sys.exit()
+    pygame.display.flip()
+
